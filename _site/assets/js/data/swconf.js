4ce7dc792316c1bb65c7c167f68a07e4f520c246
Alexander Sandercock <39815775+alex-sandercock@users.noreply.github.com>
2025-06-17 13:07:05 -0400

    config updates

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1750179223',resources: [
+      '/breeding-insight.github.io/BIGr/assets/css/jekyll-theme-chirpy.css',
+      '/breeding-insight.github.io/BIGr/',
+      
+        '/breeding-insight.github.io/BIGr/categories/',
+      
+        '/breeding-insight.github.io/BIGr/tags/',
+      
+        '/breeding-insight.github.io/BIGr/archives/',
+      
+        '/breeding-insight.github.io/BIGr/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+          'https://alex-sandercock.goatcounter.com/counter/'
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
